716f718c3d98f4d3007afc7110041a277909f427
jasonzou <jsbintask@gmail.com>
2019-01-04 15:33:15 +0800

    init projects

---
+++ basic-security/src/main/java/cn/jsbintask/basicsecurity/App.java
@@ -0,0 +1,14 @@
+package cn.jsbintask.basicsecurity;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author jsbintask@foxmail.com
+ * @date 2019/1/4 15:15
+ */
+@SpringBootApplication
+public class App {
+    public static void main(String[] args) {
+
+    }
+}
